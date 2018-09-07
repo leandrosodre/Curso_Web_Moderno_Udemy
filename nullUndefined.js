3007236682f1ecc58d297e7e9aabb14f496ce356
LeandroDesktop <leandroaugustosodre@gmail.com>
2018-09-07 13:03:28 -0300

    Objeto, Null e Undefined
    Aulas sobre Objeto, Null e Undefined.

---
+++ nullUndefined.js
@@ -0,0 +1,20 @@
+let valor
+console.log(valor)
+
+valor = null //ausência de valor
+console.log(valor)
+
+const produto = {}
+console.log(produto.preco)
+console.log(produto)
+
+produto.preco = 3.50
+console.log(produto)
+
+produto.preco = undefined //evite atribuir undefined
+console.log(!!produto.preco)
+console.log(produto)
+
+delete produto.preco
+console.log(produto)
+
